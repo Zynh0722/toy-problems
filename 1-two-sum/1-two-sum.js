84ebd4397f997e60cce176651d9d260fd982f16e
Zynh Ludwig <Zynh0722@gmail.com>
2022-08-09 18:43:49 -0700

    Time: 236 ms (15.49%), Space: 48.3 MB (5.11%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,8 +5,7 @@
  */
  var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        const secondI = i + nums.slice(i + 1).indexOf(target - num) + 1;
+        const secondI = i + nums.slice(i + 1).indexOf(target - nums[i]) + 1;
 
         if (secondI !== -1 && i !== secondI) {
             return [i, secondI];
