ef5e91c9131281cb9f630e33bb964f3bf19cf65c
Zynh Ludwig <Zynh0722@gmail.com>
2022-08-09 18:16:02 -0700

    Time: 122 ms (54.25%), Space: 48 MB (26.74%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -4,5 +4,9 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-    return s.split('').sort().join() === t.split('').sort().join();
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    return s.split('').sort().join('') === t.split('').sort().join('');
 };
