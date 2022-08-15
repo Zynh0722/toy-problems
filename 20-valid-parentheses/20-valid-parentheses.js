683f9adce85579fd504b24c77efc3c690849b7da
Zynh Ludwig <Zynh0722@gmail.com>
2022-08-15 11:43:37 -0700

    Time: 55 ms (99.41%), Space: 42.6 MB (35.93%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    const matchMap = {
+        '(': 0, ')': 0,
+        '{': 1, '}': 1,
+        '[': 2, ']': 2,
+    };
+
+    const open = ['(', '{', '['];
+
+    for (let i = 0; i < s.length; i++) {
+        if (open.indexOf(s[i]) !== -1) {
+            stack.push(s[i]);
+        } else if (matchMap[stack.at(-1)] === matchMap[s[i]]) {
+            stack.pop();
+        } else {
+            return false;
+        }
+    }
+
+    return stack.length === 0;
+};
